aa3203b8d96ab09ab4e37eaa9f7ba61c43636ad5
Austin Cooper <amcooper181@gmail.com>
2024-07-30 23:19:51 -0400

    fixed 96khz sampling

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,28 @@ void setup() {
   Serial.println("Starting I2C communication...");
   i2cInitialize();
   pcm.verifyConnection();
-
-  I2S.setPins(14, 25, 26);
+  int8_t bck = 14;
+  int8_t ws = 25;
+  int8_t dout = 13;
+  int8_t din = 26;
+  I2S.setPins(bck, ws, dout, din);
   int start = I2S.begin(I2S_MODE_STD, 96000, I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_STEREO);
 }
+const int buff_size = 4096;
+char buffer[buff_size];
+int available_bytes, read_bytes;
+
 
 void loop() {
 
+  I2S.read();
+  available_bytes = I2S.available();
+
+  read_bytes = I2S.readBytes(buffer, available_bytes < buff_size ? available_bytes : buff_size);
+
+  for (int i = 0; i < read_bytes; i += sizeof(buffer)) {
+    I2S.write((uint8_t *) &buffer[i], std::min(buff_size, read_bytes - i));
+  }
 }
 
 /// @brief Initialize i2c communication on the ESP32
